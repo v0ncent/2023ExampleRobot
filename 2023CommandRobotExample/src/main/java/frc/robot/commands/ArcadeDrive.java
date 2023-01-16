64c543f2f087f0079fc191c3cbdcf66c642466ec
v0ncent <banksvincent5@gmail.com>
2023-01-15 18:23:13 -0600

    stuff

--- 2023CommandRobotExample/src/main/java/frc/robot/commands/ArcadeDrive.java
+++ 2023CommandRobotExample/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -5,16 +5,20 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
 
 public class ArcadeDrive extends CommandBase {
   /** Creates a new ArcadeDrive. */
-  public ArcadeDrive() {
+  DriveTrain driveTrain;
+  public ArcadeDrive(DriveTrain driveTrain) {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveTrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    
 
   }
 
--- 2023CommandRobotExample/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ 2023CommandRobotExample/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
   /** Creates a new DriveTrain. */
+  
   public DriveTrain() {
     
   }
