5197bffd7e57a8dc30042dcfcbe828195027eba2
v0ncent <banksvincent5@gmail.com>
2023-01-23 17:09:56 -0600

    added neo subsystem to test

---
+++ 2023CommandRobotExample/src/main/java/frc/robot/subsystems/Neo.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Neo extends SubsystemBase {
+
+    public Neo(){
+        
+    }
+
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+    }
+    
+}
