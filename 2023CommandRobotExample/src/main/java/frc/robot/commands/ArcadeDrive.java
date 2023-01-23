5dc3bda902947bbceac1e0c705c20aa43604d28a
v0ncent <banksvincent5@gmail.com>
2023-01-23 17:42:11 -0600

    elias push

--- 2023CommandRobotExample/src/main/java/frc/robot/commands/ArcadeDrive.java
+++ 2023CommandRobotExample/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -13,6 +13,7 @@ public class ArcadeDrive extends CommandBase {
   /** Creates a new ArcadeDrive. */
   private DriveTrain driveTrain;
   private Joystick joyStick;
+  private double lastSpeed;
   public ArcadeDrive(DriveTrain driveTrain, Joystick joystick) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(driveTrain);
@@ -24,16 +25,29 @@ public class ArcadeDrive extends CommandBase {
   @Override
   public void initialize() {
     driveTrain.arcadeDrive(0, 0);
+    lastSpeed = 0;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
         //define our movespeed and rotatespeed variables with the get raw method of the joystick
-        double moveSpeed = -joyStick.getRawAxis(Constants.OperatorConstants.JOYSTICK_MOVE_AXIS);//1 to -1
-        double rotateSpeed = joyStick.getRawAxis(Constants.OperatorConstants.JOYSTICK_ROTATE_AXIS); // 1 to -1
-        //call arcade drive method
-        driveTrain.arcadeDrive(moveSpeed, rotateSpeed);
+        double moveSpeed = -joyStick.getRawAxis(Constants.OperatorConstants.JOYSTICK_MOVE_AXIS);//1 to -1 x
+        double rotateSpeed = joyStick.getRawAxis(Constants.OperatorConstants.JOYSTICK_ROTATE_AXIS); // 1 to -1 x2
+
+        double acceleration = 0.05;
+        if (Math.abs(moveSpeed - lastSpeed) > acceleration){
+          if (moveSpeed > lastSpeed){
+            driveTrain.arcadeDrive(acceleration + lastSpeed, rotateSpeed);
+            lastSpeed = lastSpeed + acceleration;
+          } else {
+            driveTrain.arcadeDrive(lastSpeed - acceleration, rotateSpeed);
+            lastSpeed = lastSpeed - acceleration;
+          }
+        } else {
+          driveTrain.arcadeDrive(moveSpeed, 0);
+          lastSpeed = moveSpeed;
+        }
 
   }
 
@@ -41,6 +55,7 @@ public class ArcadeDrive extends CommandBase {
   @Override
   public void end(boolean interrupted) {
     driveTrain.arcadeDrive(0, 0); // when done moving, stop
+    lastSpeed = 0;
   }
 
   // Returns true when the command should end.
